5f5f6918b16cabe551da22211b39d2c6bc87173f
Camila <camiilacguimaraes@gmail.com>
2020-09-05 23:39:57 -0300

    Footer
    Footer fixo

--- src/componentes/Footer/index.js
+++ src/componentes/Footer/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { FooterBase, Texto, Direitos } from './styles';
+import { FooterBase, Texto, Direitos, Link, Span } from './styles';
 
 function Footer() {
   return (
     <FooterBase>
-      <Texto>camiilacguimaraes@gmail.com</Texto>
-      <Texto>(31) 9.9176-2261</Texto>
+      <Texto>camiilacguimaraes@gmail.com <Span> · </Span> <Link href="https://api.whatsapp.com/send?phone=5531991762261" target="_blank">(31) 9.9176-2261</Link></Texto>
       <Direitos>© 2020 Todos os direitos reservados.</Direitos>
     </FooterBase>
   );
--- src/componentes/Footer/styles.js
+++ src/componentes/Footer/styles.js
@@ -10,16 +10,34 @@ export const FooterBase = styled.footer`
   padding-bottom: 15px;
   color: var(--white);
   text-align: center;
+  font-size: 1.13vw;
+
+  z-index: 100;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+
   @media (max-width: 800px) {
     margin-bottom: 50px;
   }
 `;
 
 export const Direitos = styled.p`
-  margin: 2vw 0vw 1vw 0vw;
+  margin: 1.2vw 0vw 0.1vw 0vw;
+  font-size: 1vw;
 `;
 
 export const Texto = styled.p`
   margin: .5vw 0vw;
 `;
 
+export const Link = styled.a`
+  margin: .5vw 0vw;
+  text-decoration: none;
+`;
+
+export const Span = styled.span`
+  margin: 0vw .4vw;
+  font-weight: bold;
+`;
