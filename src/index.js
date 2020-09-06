63e292c48b789d8a06c8cbe0a895c18836b62082
Camila <camiilacguimaraes@gmail.com>
2020-09-05 21:35:42 -0300

    Public

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
+/*arquivo que inicializa o react*/
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './pages/Home/index';
+import Contato from './pages/Contato';
+import SobreMim from './pages/SobreMim';
+/*Faz o caminho entre as páginas*/
+import { BrowserRouter, Switch, Route } from "react-router-dom"; 
+import PaginaErro from './pages/Erro';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Switch>
+      {/*<Route path="/" component = { Home } exact/> 
+      <Route path = "/Contato" component = { Contato }/>*/}
+      <Route path="/" component = { SobreMim }/>
+      <Route component = { PaginaErro }/>
+    </Switch>       
+  </BrowserRouter>,
+  document.getElementById('root') /*renderiza o react no root do index.html*/
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
