645617b75ca299372f4cbedcc0c407f78cd58e84
Camila <camiilacguimaraes@gmail.com>
2021-04-13 23:34:53 -0300

    FOOTER: implementação inicial

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+import styles from '../styles/components/Footer.module.css';
+
+
+function Footer() {
+  return (
+    <div className={ styles.footerBase }>
+      <p className={ styles.texto }>
+        camiilacguimaraes@gmail.com 
+        <span className={ styles.span }> · </span> 
+        (31) 9.9176-2261
+      </p>
+      <p className={ styles.direitos }> © 2020 Todos os direitos reservados.</p>
+    </div>
+  );
+}
+
+export default Footer;
