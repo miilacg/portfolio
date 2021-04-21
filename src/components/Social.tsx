799aa67d81b6017a6e4e3e44b0a828316619b06f
Camila <camiilacguimaraes@gmail.com>
2021-04-20 22:44:43 -0300

    HOME: implementação da página principal

---
+++ src/components/Social.tsx
@@ -0,0 +1,19 @@
+import styles from '../styles/components/Social.module.css';
+
+export default function Social() { 
+  return (   
+    <div className={ styles.social }>
+      <a className={ styles.icon } href="https://linkedin.com/in/miilacg" >
+        <i className="fab fa-linkedin"></i>
+      </a>
+      
+      <a className={ styles.icon } href="https://github.com/miilacg" >
+        <i className="fab fa-github"></i>
+      </a> 
+
+      <a className={ styles.icon } href="https://instagram.com/miilacg" >
+        <i className="fab fa-instagram"></i>
+      </a>        
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
 import PageDefault from '../components/PageDefault';
+import Social from '../components/Social';
 
-import styles from '../styles/pages/SobreMim.module.css';
+import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
-   return (  
-      <PageDefault href='/'>                  
-         <div className={ styles.sobreMim }>            
-            <div className={ styles.imagem }>
-               <img src="../../imagens/camila.jpg"></img>
-            </div>
-            <div className={ styles.texto }>
-               <h1>Olá</h1>
-               <p>Tenho 24 anos, sou formada em Ciência da Computação pela UFV e apaixonada por tecnologia.</p>
-               <p>Minha paixão pelo desenvolvimento web deu inicio quando comecei a implementar meu trabalho de conclusão de curso. Depois eu me especializei mais na área fazendo um curso de Front-end na Gama.</p>
-               <p>Eu posso transformar seu negócio em um site, representando sua marca de maneira inteligente e um ótimo relacionamento com os clientes.&nbsp;</p>
-               <p>Se você possui um negócio ou uma ideia, entre em contato, tenho certeza que podemos fazer algo incrível.</p>
-            </div>
-         </div> 
-      </PageDefault>                      
-   );
+  return (  
+    <PageDefault href='/'>       
+      <div className={ styles.texto }>
+        <h2>Eu sou</h2>      
+        <h1>Camila Guimarães</h1>
+        <h3>desenvolvedora web</h3>
+      </div>  
+
+      <Social />      
+    </PageDefault>                      
+  );
 }
