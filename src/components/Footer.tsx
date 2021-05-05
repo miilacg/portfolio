0e543cad4e13262d85ec9df2b404429954ee987e
Camila Guimarães <camiilacguimaraes@gmail.com>
2021-05-04 22:37:34 -0300

    FOOTER: alteração de tags

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,14 @@
 import styles from '../styles/components/Footer.module.scss';
 
-function Footer() {
+export default function Footer() {
   return (
-    <div className={ styles.footerBase }>
+    <footer className={ styles.footerBase }>
       <p className={ styles.texto }>
         camiilacguimaraes@gmail.com 
         <span className={ styles.span }> · </span> 
         (31) 9.9176-2261
       </p>
       <p className={ styles.direitos }> © 2020 Todos os direitos reservados.</p>
-    </div>
+    </footer>
   );
 }
-
-export default Footer;
