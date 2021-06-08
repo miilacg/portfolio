50537d43b66329194ab4b062eeed9ae8ebe6ccf3
Camila Guimarães <38961741+miilacg@users.noreply.github.com>
2021-06-08 16:29:34 -0300

    PROJETOS: versão sem carousel

--- src/pages/Projetos.tsx
+++ src/pages/Projetos.tsx
@@ -1,66 +1,54 @@
 import { GetStaticProps } from 'next';
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+import Link from 'next/link';
 
 import { api } from '../services/api';
-
 import styles from '../styles/pages/Projetos.module.scss';
 
 
 
-type Projeto = { 
+type Project = { 
   id: string;
   category: string;
   title: string;
   description: string;
+  languages: string;
   image: string;
   url: string;
   publishedAt: string;
 } 
 
-type ProjetosProps = { 
-  projetos: Projeto[];
+type ProjectsProps = { 
+  projects: Project[];
 }
 
-
-
-export default function Projetos({ projetos }: ProjetosProps) {
+export default function Projetos({ projects }: ProjectsProps) {
   return (  
-    <main className={ styles.mainProjetos }>       
+    <main className={ styles.mainProjects }>       
       <h1>Portfólio</h1>
-      <h2>Alguns dos meus projetos</h2>
+      <h2>Alguns dos meus projetos</h2>   
 
-      <section>
-        <table cellSpacing={ 0 }>
-          <thead>
-            <tr>
-              <th></th>
-              <th>Podcast</th>
-              <th>Integrantes</th>
-              <th>Data</th>
-            </tr>
-          </thead>
-          <tbody>
-            { projetos.map((projeto, index) => {
-              return (
-                <tr key={ projeto.id }>
-                  <td style={{ width:72 }}>
-                    { projeto.category }
-                  </td> 
-                  <td>
-                    { projeto.title }
-                  </td>    
-                  <td>
-                    { projeto.description }
-                  </td> 
-                  <td>
-                    { projeto.url }
-                  </td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
+      <section>        
+        { projects.map((projects, index) => {
+          return (
+            <div key={ projects.id } className={ styles.project }>
+              <div className={ styles.description }>
+                <h3> { projects.title } </h3>
+                <div dangerouslySetInnerHTML={{ __html: projects.description }} />  
+                <p> <strong>Linguagens e tecnologias utilizadas:</strong> { projects.languages } </p>
+                
+                <Link href={ projects.url }>
+                  <a> Visite o site </a>
+                </Link>
+              </div>  
+              
+              <div className={ styles.image }>
+                <img src={ projects.image } alt={ projects.title } /> 
+              </div>            
+            </div>
+          )
+        })}
       </section>
     </main>                      
   );
@@ -69,26 +57,27 @@ export default function Projetos({ projetos }: ProjetosProps) {
 export const getStaticProps: GetStaticProps = async () => { 
   const { data } = await api.get('projetos', {
     params: {
-      _sort: 'published_at',
+      _sort: 'publishedAt',
       _order: 'desc'
     }
   })  
 
-  const projetos = data.map(projeto => {
+  const projects = data.map(project => {
     return {
-      id: projeto.id,
-      category: projeto.category,
-      title: projeto.title,
-      description: projeto.description,
-      image: projeto.image,
-      url: projeto.url,
-      publishedAt: format(parseISO(projeto.publishedAt), 'd MMM yy', { locale: ptBR }),
+      id: project.id,
+      category: project.category,
+      title: project.title,
+      description: project.description,
+      languages: project.languages,
+      image: project.image,
+      url: project.url,
+      publishedAt: format(parseISO(project.publishedAt), 'd MMM yy', { locale: ptBR }),
     };
   }) 
       
   return {
     props: { 
-      projetos
+      projects
     },
     revalidate: 60 * 60 * 24, //só executa 1 vezes por dia
   }
