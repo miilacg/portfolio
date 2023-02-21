cde3dd3544eb38e97842ad62bd40f2cb26a6da67
Camila Guimarães <camiilacguimaraes@gmail.com>
2023-02-21 18:31:20 -0300

    :sparkles: feat: responsividade da página de projetos

--- src/pages/Projetos.tsx
+++ src/pages/Projetos.tsx
@@ -4,13 +4,33 @@ import Carousel from 'react-bootstrap/Carousel';
 
 import Typography from '@mui/material/Typography';
 
+import { makeStyles } from '@mui/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+import { isMobile } from 'react-device-detect';
 import { projetos } from '../services/projetos';
 
 import styles from '../styles/pages/Projetos.module.scss';
 
 
+const useStyles = makeStyles((theme) => ({
+  carousel: {
+    '& .carousel-item > div': {
+      padding: isMobile && '0',
+    },
+    '& .carousel-control-prev': {
+      display: isMobile && 'none',
+    },
+    '& .carousel-control-next': {
+      display: isMobile && 'none',
+    },
+  }
+}));
 
 export default function Projetos() {
+  const classes = useStyles();
+  const isSmall = useMediaQuery('(max-width: 800px)');
+
   const categories = Object.keys((projetos.map((project) => { return project.category })).reduce((acc, curr) => (acc[curr] = '', acc), {}));
 
 
@@ -24,13 +44,19 @@ export default function Projetos() {
           <section key={category}>
             <Typography variant='h2'>{ category }</Typography>
 
-            <Carousel indicators={ false } interval={ 50000 }>
+            <Carousel className={ classes.carousel } indicators={ false } interval={ 50000 }>
               { projetos.map((project) => {
                 return (
                   category == project.category && (
                     <Carousel.Item key={ project.id }>
                       <div className={ styles.project }>
-                        <div className={ styles.description } style={{ width: project.image ? '50%' : '100%', margin: project.image ? '0 1.5625rem 0 0' : '0' }}>
+                        <div
+                          className={ styles.description }
+                          style={{
+                            width: !isSmall && project.image ? '50%' : '100%',
+                            margin: !isSmall && project.image ? '0 1.5625rem 0 0' : '0'
+                          }}
+                        >
                           <Typography variant='h3'> { project.title } </Typography>
 
                           <div dangerouslySetInnerHTML={{ __html: project.description }} />  
@@ -44,7 +70,7 @@ export default function Projetos() {
                           </Link>
                         </div> 
                         
-                        { project.image && (
+                        { !isSmall && project.image && (
                           <div className={ styles.image }>
                             <img src={ project.image } alt={ project.title } /> 
                           </div>  
