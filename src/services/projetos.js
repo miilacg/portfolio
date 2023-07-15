a2bee0487145923998c4e0445476223cf6c26023
Camila Guimarães <camiilacguimaraes@gmail.com>
2023-07-15 17:26:22 -0300

    novos projetos

--- src/services/projetos.js
+++ src/services/projetos.js
@@ -7,17 +7,17 @@ export const projetos = [
     "languages": "CSS e HTML",
     "image": "https://user-images.githubusercontent.com/38961741/183268426-515cc0d4-9e28-4e21-a93f-4860dd804884.png",
     "url": "https://github.com/miilacg/ong-produzindoAlegria",
-    "publishedAt": "2020-02-14 16:35:40"
+    "publishedAt": "2023-04-01 12:35:40"
   },
   {
     "id": 2,
     "category": "Web",
     "title": "Moveit",
     "description": "<p>O move.it é um app que une a técnica de Pomodoro com a realização de exercícios físicos para quem passa muito tempo na frente do computador.</p><p>Esse projeto foi desenvolvido durante a trilha de React, na quarta edição da NLW.</p>",
-    "languages": "React com TypeScript e CSS",
+    "languages": "React com TypeScript, Next.JS e CSS",
     "image": "https://user-images.githubusercontent.com/38961741/183268773-cdd6122f-8925-4835-af60-2e8eb017df47.png",
-    "url": "https://miilacg-moveit-n6ca2thbs-miilacg.vercel.app/",
-    "publishedAt": "2021-02-27 16:35:40"
+    "url": "https://miilacg-moveit.vercel.app/",
+    "publishedAt": "2023-04-01 13:50:00"
   },
   {
     "id": 3,
@@ -27,7 +27,7 @@ export const projetos = [
     "languages": "Spring, Java, React com TypeScript, HTML e CSS",
     "image": "https://raw.githubusercontent.com/miilacg/projeto-sds3/9ce6a7c4a6c0e587536a08f1e63c8b6613279328/frontend/src/assets/img/ds-dark.svg",
     "url": "https://dsvendas-miilacg.netlify.app/",
-    "publishedAt": "2021-05-08 16:35:40"
+    "publishedAt": "2023-04-01 14:01:00"
   },
   {
     "id": 4,
@@ -67,7 +67,7 @@ export const projetos = [
     "languages": "React com JavaScript, HTML e CSS",
     "image": "https://user-images.githubusercontent.com/38961741/183268828-5d2ed2b1-ff05-454d-ac9d-ccacb8c12eea.png",
     "url": "https://github.com/miilacg/jogoDaVelha",
-    "publishedAt": "2021-06-11 15:11:40"
+    "publishedAt": "2021-12-30 15:11:40"
   },
   {
     "id": 8,
@@ -77,7 +77,7 @@ export const projetos = [
     "languages": "React, Meteor, HTML, CSS e MongoDB",
     "image": "https://raw.githubusercontent.com/miilacg/toDoList/main/public/images/interface.png",
     "url": "https://github.com/miilacg/helloMeteor",
-    "publishedAt": "2021-06-04 16:11:40"
+    "publishedAt": "2021-06-09 16:11:40"
   },
   {
     "id": 9,
@@ -85,7 +85,25 @@ export const projetos = [
     "title": "Letmeask",
     "description": "<p>Letmeask é perfeito para criadores de conteúdos poderem criar salas de Q&A com o seu público, de uma forma muito organizada e democrática.</p>",
     "languages": "React, Firebase, TypeScript e SCSS",
-    "url": "https://github.com/miilacg/nlw6/tree/main/letmeask",
+    "url": "https://letmeask-miilacg.web.app/",
     "publishedAt": "2021-06-26 16:11:40"
+  },
+  {
+    "id": 10,
+    "category": "Web",
+    "title": "NPS-NLW",
+    "description": "<p>API que consiste em calcular o NPS da empresa. Nele fazemos o cadastro de usuários, cadastro de pesquisas e envio de e-mail para os usuários responderem as pesquisas de satisfação e com isso podemos realizar o cálculo do NPS</p>",
+    "languages": "TypeScript, TypeORM, Express, Ethereal-Email",
+    "url": "https://github.com/miilacg/nlw4/tree/master/api",
+    "publishedAt": "2023-04-01 13:41:40"
+  },
+  {
+    "id": 11,
+    "category": "Web",
+    "title": "My finance",
+    "description": "<p>Sistema financeiro pessoal</p>",
+    "languages": "ASP.Net, C#, HTML, SQL Server, Entity Framework",
+    "url": "https://github.com/miilacg/myFinance-web/tree/main",
+    "publishedAt": "2023-07-15 17:15:40"
   }
 ]
